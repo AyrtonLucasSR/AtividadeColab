a00bcbef961a440936d80dcf07ee21e4900147e6
Regianecossta <113606228+Regianecossta@users.noreply.github.com>
2022-12-05 09:03:47 -0300

    adicinado imagem

---
+++ img.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Atividade 2</title>
+    </head>
+    <body>
+    <center>
+        <font color="Maroon" size="5">Regiane Costa</font>
+        <br><br>
+        <form>
+            <table border="1" align="center" width="450">
+                <tr>
+                    <td align="center" colspan="2" bgcolor="DeepPink"><font color="black">Tarsila do Amaral</td>                    
+                </tr>
+                <tr align="center" >
+                    <td >Obras</td>
+                    <td>Quadro</td>
+                </tr>
+                <tr align="center">
+                    <td >Operários</td>
+                    <td><img src="Operarios.jpg" width="250"></td>
+                </tr>
+                <tr align="center">
+                    <td>Abapuru</td>
+                    <td><img src="Abaporu.jpg" width="150" >
+                </tr>
+            </table>
+        </form>
+    </center>
+
+    </body>
+</html>
